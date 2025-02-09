999cab77cc9b8764e63e60da46f04b653faa1a5a
KALEKRSTOS731 <kalekirstoszeru@gmail.com>
2025-02-09 01:18:03 -0800

    changes

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ document.addEventListener("DOMContentLoaded", function(){
     //....
 });
 const form = document.getElementById("registration-form");
+let isValid = true;
 const feedbackDiv= document.getElementById("form-feedback");
+
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password =document.getElementById("password");
